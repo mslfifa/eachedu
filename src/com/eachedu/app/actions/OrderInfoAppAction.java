af337614932e89d646852f9d5ec0f607c3079948
mslfifa <mslfifa@163.com>
2015-08-01 23:13:35 +0800

    fffff

---
+++ src/com/eachedu/app/actions/OrderInfoAppAction.java
@@ -0,0 +1,12 @@
+package com.eachedu.app.actions;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+import com.eachedu.web.actions.BaseAction;
+
+@Controller("orderInfoAppAction")
+@Scope("prototype")
+public class OrderInfoAppAction extends BaseAction{
+
+}
--- src/com/eachedu/app/actions/TeacherAppAction.java
+++ src/com/eachedu/app/actions/TeacherAppAction.java
@@ -1,10 +1,5 @@
 package com.eachedu.app.actions;
 
-import java.net.URLDecoder;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import javax.annotation.Resource;
 
 import org.slf4j.Logger;
@@ -12,10 +7,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
-import com.eachedu.exception.ServiceException;
 import com.eachedu.service.TeacherInfoService;
 import com.eachedu.web.actions.BaseAction;
-import com.eachedu.web.vo.PagerVO;
 @Controller("teacherAppAction")
 @Scope("prototype")
 public class TeacherAppAction extends BaseAction{
